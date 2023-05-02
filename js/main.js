884fb1068491f8799ca99ea708c9adc8eb17d311
HYUN125 <dodenehxhfl@naver.com>
2023-05-02 15:34:41 +0900

    워너두 칠드런스 뮤지엄

--- js/main.js
+++ js/main.js
@@ -1,54 +1,9 @@
 $(document).ready(function(){
-    
-
-
-    const train = $('.orange .train .maintrain')
-    // console.log(train)
-    const cre_img = $('.creative .cre-box > img')
-    const cre_txt = $('.creative .cre-box > span')
-    const cre_m_img = $('.creative .cre-img')
-    const po_img = $('.potential .po-img')
-    const po_txtbox = $('.potential .po-box > img')
-    const po_txt = $('.potential .po-box > span ')
-
-    $(window).scroll(function(){
-        let sct = $(window).scrollTop();
-        // console.log(sct)
-        if(sct > 500){
-            train.addClass('on')
-        }else{
-            
-        }
-        if(sct > 3400){
-            cre_img.addClass('on')
-            cre_txt.addClass('on')
-            cre_m_img.addClass('on')
-        }else{
-            // $(window).resize(function(){
-            //     let reSize = window.innerWidth;
-            //     // console.log(reSize)
-            //     if(reSize<1100){
-            //         cre_img.removeClass('on')
-            //         cre_txt.removeClass('on')
-            //         cre_m_img.removeClass('on')
-            //     }
-            // })
-        }
-        if(sct > 4137){
-            po_img.addClass('on')
-            po_txtbox.addClass('on')
-            po_txt.addClass('on')
-        }
-    })
-    $(window).resize(function(){
-        let reSize = window.innerWidth;
-        // console.log(reSize)
-        $(window).scroll(function(){
-            let sctt = $(window).scrollTop();
-            console.log(sctt)
-            if(sctt=200 && reSize < 900){
-                train.addClass('on')
-            }
-        })
-    })
+    AOS.init({
+        once: false,
+        disable: function () {
+            let m_width = 650;
+            return window.innerWidth < m_width;
+          }
+    });    
 })
